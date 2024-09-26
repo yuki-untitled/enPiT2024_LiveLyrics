f109253c080ba160c33253c377f777404743edcc
yuki <108281977+yuki-untitled@users.noreply.github.com>
2024-09-26 07:34:19 +0000

    発表用に修正しました。

--- rubi.js
+++
@@ -1,4 +0,0 @@
-const text=document.querySelector(`.lyrics`).innerText;
-
-kuromoji.builder({ dicPath: "node_modules/kuromoji/dict/" }).build(function (err, tokenizer) {     if (err) throw err;     const tokens = tokenizer.tokenize(text);  // 取得したテキストを解析  
-                                                                                              tokens.forEach(token => {         console.log(token.surface_form, token.reading); }); });
