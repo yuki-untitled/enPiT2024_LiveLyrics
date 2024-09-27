df646728073ee15211db5c5c0ab33e099d7b3e80
yuki-untitled <n2i5s4h1@icloud.com>
2024-09-27 14:04:09 +0900

    新しく枝を作成しました

--- index.js
+++ index.js
@@ -6,7 +6,6 @@
 const express = require("express");
 const app = express();
 const http = require("http").createServer(app);
-const WebSocket = require("ws");
 
 // "/"にアクセスがあったらindex.htmlを返却
 app.use("/", express.static(__dirname + "/"));
